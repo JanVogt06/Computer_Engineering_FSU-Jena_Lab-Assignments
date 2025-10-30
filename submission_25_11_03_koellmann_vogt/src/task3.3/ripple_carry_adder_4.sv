5f5918f623208e10c77584017b7abe1b3543ad6e
Jan Vogt <jan.vogt06@web.de>
2025-10-30 17:07:27 +0100

    sub 3 tasks src

--- submission_25_11_03_koellmann_vogt/src/task3.3/ripple_carry_adder_4.sv
+++ submission_25_11_03_koellmann_vogt/src/task3.3/ripple_carry_adder_4.sv
@@ -1,22 +1,3 @@
-module full_adder(
-        input  wire i_a,
-                    i_b,
-                    i_carry_in,
-        output wire o_s,
-                    o_carry_out
-    );
-
-    xor gate_s(o_s, i_a, i_b, i_carry_in);
-
-    wire l_ab, l_ac, l_bc;
-
-    and gate_ab(l_ab, i_a, i_b);
-    and gate_ac(l_ac, i_a, i_carry_in);
-    and gate_bc(l_bc, i_b, i_carry_in);
-
-    or gate_cout(o_carry_out, l_ab, l_ac, l_bc);
-endmodule
-
 /**
  * 4-bit ripple-carry adder.
  *
